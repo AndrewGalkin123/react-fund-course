defe4f424ccd7b2b68c8ab231f7b715840b18fb0
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-25 16:45:06 +0300

    react props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react"
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import "./styles/App.css"
 
 function App() {
     return (
         <div className="App">
-           <Counter/>
-          <ClassCounter/>
+            <Counter />
+            <ClassCounter />
+            <PostItem post={{id: 1, title: "Javascript", body: "description"}} />
+
+            
+
         </div>
     )
 }
