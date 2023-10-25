a267233ec547593d54dfff0126aa8b349b968303
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-25 21:16:48 +0300

    postlist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react"
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-import PostItem from "./components/PostItem";
+
 import "./styles/App.css"
+import PostList from "./components/PostList";
 
 function App() {
+    const [posts, setPosts] = useState([
+        { id: 1, title: "Javascript", body: "description" },
+        { id: 2, title: "Javascript 2", body: "description" },
+        { id: 3, title: "Javascript 3", body: "description" },
+
+    ])
+   
     return (
         <div className="App">
             <Counter />
             <ClassCounter />
-            <PostItem post={{id: 1, title: "Javascript", body: "description"}} />
-
-            
+            <form>
+                <input type="text" placeholder="Название поста"></input>
+                <input type="text" placeholder="Описание поста"></input>
+                <button>Создать</button>
+            </form>
+            <PostList title="Посты про JS" posts={posts}/>
+          
 
         </div>
     )
