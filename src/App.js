ab079a194e8cadd29d8cbee20ed0721fcc3b3e16
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-26 12:54:06 +0300

    react-fund-course lesson 3

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
 import React, { useState, useRef } from "react"
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css"
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        { id: 1, title: "Javascript", body: "description" },
-        { id: 2, title: "Javascript 2", body: "description" },
-        { id: 3, title: "Javascript 3", body: "description" },
+        { id: 1, title: "ff", body: "description" },
+        { id: 2, title: "rr 2", body: "description" },
+        { id: 3, title: "dd 3", body: "description" },
 
     ])
-    const [post, setPost] = useState({ title: '', body: '' })
+
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+    }
 
     // const bodyInputRef = useRef()  получаем доступ напрямую к DOM элементу
-    const addNewPost = (e) => {
-        e.preventDefault()
-        setPosts([...posts, {...post, id: Date.now()}])
-        setPost({ title: '', body: '' })
-    }
+
     return (
         <div className="App">
-            <form>
-                {/* Управляемый компонент */}
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost({ ...post, title: e.target.value })}
-                    type="text"
-                    placeholder="Название поста"
+            <PostForm create={createPost} />
+            <hr style={{ margin: "15px 0" }}></hr>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        { value: 'title', name: "По названию" },
+                        { value: 'body', name: "По описанию" }
+                    ]}
                 />
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost({ ...post, body: e.target.value })}
-                    type="text"
-                    placeholder="Описание поста"
-                />
-                {/* Неуправляемый компонент */}
-                {/* <MyInput ref={bodyInputRef} type="text" placeholder="Описание поста" /> */}
-                <MyButton onClick={addNewPost}>Создать</MyButton>
-            </form>
-            <PostList title="Посты про JS" posts={posts} />
+            </div>
+            {posts.length
+                ?
+                <PostList title="Посты про JS" remove={removePost} posts={posts} />
+                :
+                <h1 style={{ textAlign: "center" }}>
+                    Посты не найдены!
+                </h1>
+            }
+
 
 
         </div>
